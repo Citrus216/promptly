d000745072a252c24c9b2c16500760f20948e73b
RyanTavol <121977925+RyanTavol@users.noreply.github.com>
2024-06-05 21:32:53 -0400

    forgot to save a file first

--- backend/src/main/java/com/promptly/backend/database/schemas/Prompt.java
+++ backend/src/main/java/com/promptly/backend/database/schemas/Prompt.java
@@ -1,9 +1,36 @@
 package com.promptly.backend.database.schemas;
 
+import java.util.Date;
+
 @Document(collection = "prompts")
 public class Prompt {
     @Id
-    String promString;
+    private String id;
+    private Date dateCreated;
+    private String promptString;
 
     // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getPromptString() {
+        return promptString;
+    }
+
+    public void setPromptString(String promptString) {
+        this.promptString = promptString;
+    }
 }
