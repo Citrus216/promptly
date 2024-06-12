f127ed6f9351784d9fddc91f972c8574d72349b0
John Lehmann <john.d.lehmann@gmail.com>
2024-06-11 20:38:31 -0400

    moved example files to ./*-example directories

--- frontend/app-example/+not-found.tsx
+++ frontend/app-example/+not-found.tsx
@@ -1,8 +1,8 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components-example/ThemedText';
+import { ThemedView } from '@/components-example/ThemedView';
 
 export default function NotFoundScreen() {
   return (
