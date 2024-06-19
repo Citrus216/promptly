ee2f00f1461049cebf578ad72de6da1cb6803d3a
John Lehmann <john.d.lehmann@gmail.com>
2024-06-18 20:32:05 -0400

    attempts at fixing mongo error

--- backend/src/main/java/com/promptly/backend/BackendApplication.java
+++ backend/src/main/java/com/promptly/backend/BackendApplication.java
@@ -2,8 +2,9 @@ package com.promptly.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
